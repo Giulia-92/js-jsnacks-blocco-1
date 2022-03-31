4f53e5062953d5d797a7ee88671662f2790de39d
Giulia-92 <cionco.giulia@gmail.com>
2022-03-31 16:25:19 +0200

    js-jsnacks-blocco-1

--- JSnack 1/script.js
+++ JSnack 1/script.js
@@ -1,4 +1,21 @@
-const nomiInlista = []
+const lista = ['invitato1,invitato2,invitato3,invitato4,invitato5,invitato6']
+document.getElementById('btn').addEventListener('click', function(){
+    const nomeAccesso = document.getElementById('name').value
+    let nomeInlista = false;
+    for (let i = 0; i < lista.length; i++){
+        const nomeTrovato = lista[i]
+        if(nomeAccesso === lista){
+            nomeInlista = true;
+        } else{
+            nomeInlista = false;
+        }
+        if(nomeInlista){
+            prompt('Sei in lista!')
+        } else{
+            prompt('Non sei in lista!')
+        }
+    }
+})
 
 
 
