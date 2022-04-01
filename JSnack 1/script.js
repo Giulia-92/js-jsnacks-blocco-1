71bbc4ccb41b727461199805e7169881ba447d90
Giulia-92 <cionco.giulia@gmail.com>
2022-04-01 10:06:11 +0200

    js-jsnacks-blocco-1

--- JSnack 1/script.js
+++ JSnack 1/script.js
@@ -1,66 +1,11 @@
 const lista = ['invitato1,invitato2,invitato3,invitato4,invitato5,invitato6']
-document.getElementById('btn').addEventListener('click', function(){
+document.getElementById('btn').addEventListener('click', function () {
     const nomeAccesso = document.getElementById('name').value
     let nomeInlista = false;
-    for (let i = 0; i < lista.length; i++){
+    for (let i = 0; i < lista.length; i++) {
         const nomeTrovato = lista[i]
-        if(nomeAccesso === lista[i]){
-            nomeInlista = true;
-        } else{
-            nomeInlista = false;
-        }
-        if(nomeInlista){
-            prompt('Sei in lista!')
-        } else{
-            prompt('Non sei in lista!')
+        if (nomeAccesso === lista[i]) {
+            nomeInlista = true
         }
     }
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const mail = prompt ("inserisci mail");
-let arrayMail = ['mail1@mail.it','mail2@mail.it','mail3@mail.it','mail4@mail.it','mail5@mail.it'];
-document.getElementById('check').addEventListener('click',function(){
-const utente = document.getElementById('accesso-email').value
-let convalida = false;
-for (let i = 0; i< arrayMail.length; i++){
-    if(mail === arrayMail){
-        convalida = true
-        
-    } 
-    if(convalida){
-        alert('accesso consentito')
-    }else {
-        alert('accesso negato')
-    }
-}
-})*/
+        })
