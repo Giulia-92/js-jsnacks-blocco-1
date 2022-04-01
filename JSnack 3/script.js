1da244a2589406d3c5c9913fab9508c0e403ab87
Giulia-92 <cionco.giulia@gmail.com>
2022-04-01 11:20:26 +0200

    js-jsnacks-blocco-1

--- JSnack 3/script.js
+++ JSnack 3/script.js
@@ -1,7 +1,11 @@
+let conteggio;
+let numeri = prompt('inserisci un numero di 4 cifre' );
+if(numeri.legth == 4){
+    conteggio = numeri.split("");
 let somma = 0;
-let codice = [30,20,5,12];
 let index = 0;
-while( somma < 67){
-    somma = somma + codice [i];
+while(index < conteggio.length){
+    somma += parseInt(conteggio[index]);
     index ++;
+}
 }
